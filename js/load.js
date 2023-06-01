407ccd75a1693f6d61e5272c81ca86aeee232bc6
HumbleG0d <sergiouni2000@gmail.com>
2023-06-01 10:58:02 -0500

    add the display of the letter after the page loads

--- js/load.js
+++ js/load.js
@@ -1,10 +1,12 @@
 function loadGame () {
 	window.addEventListener('load' , () => {
 		const loader = document.getElementById('loader')
+		const section = document.getElementById('pokemon__play')
 		const time_load = 3000
 
 		setTimeout(()=>{
 			loader.style.display = 'none'
+			section.style.display = 'flex'
 		} , time_load)
 	})
 }
