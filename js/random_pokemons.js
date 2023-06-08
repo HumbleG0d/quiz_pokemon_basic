03b782f15f61e2c1c4776e0fb870cda2fc0dc3cd
HumbleG0d <sergiouni2000@gmail.com>
2023-06-08 06:05:40 -0500

    change the use of the getRandomPokem function

--- js/random_pokemons.js
+++ js/random_pokemons.js
@@ -1,7 +1,9 @@
+//? La funcion revisira un array de 20 elemntos de estos 20 elementos quiero que salga aleatoriamente sin repetirse , babara un evento que me permita elejir otro  pokemon pero sin repeticion despues de salir los 20 se regresara a la API por otros 20 pokemones.AHora con cadap okemons elejido se le psara por una funcion  que nos mostrara todos la info del pokemon.
+import { infoPokemon } from './info_pokemon.js'
 
 export const getRandomPokemon = (data) => {
-	const array_pokemons = data.results.map(element => element.name)
-	const index_ran = Math.floor(Math.random() *(array_pokemons.length + 1))
-	return array_pokemons[index_ran]
+	
+	const index_ran = Math.floor(Math.random() *(data.length + 1))
+	infoPokemon(data[index_ran])
 }
 
