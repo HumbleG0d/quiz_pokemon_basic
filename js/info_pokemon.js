73bf5bc2bce5064f1c8a2a4466c975d6dfb7e0ec
HumbleG0d <sergiouni2000@gmail.com>
2023-06-08 06:02:31 -0500

    add the file info pokemon that will show us both the image with the stats of the pokemon

---
+++ js/info_pokemon.js
@@ -0,0 +1,9 @@
+export const infoPokemon = (pokemon) => {
+	const img = document.getElementById('pokemon__img')
+	img.style.backgroundImage = `url(${pokemon.sprites.other.dream_world.front_default})`
+
+	const td_table = document.querySelectorAll('td')
+	td_table.forEach((element , index) => {
+		element.style = `--size: calc(${pokemon.stats[index].base_stat}/300)`
+	})
+}
